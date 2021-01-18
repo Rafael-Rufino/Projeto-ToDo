fb1a921eb6baaa00b9e6941d1b3ba5dfacb6a840
Rafael Rufino <rafaelrufino.52@gmail.com>
2021-01-18 18:24:14 -0300

    Iniciando o model, view e template, trazendo as informações oões do banco para template

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+
+
+    STATUS =(
+        ('Realizando', 'Fazendo'),
+        ('Pendente', 'Pendente'),
+        ('Concluido', 'Concluida'),
+    )
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    done = models.CharField(
+            max_length = 10, 
+            choices=STATUS,
+    )
+    create_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
+
+
+
+    def __str__(self):
+        return self.title 
+        
+
+
+
+
+
